92efb73fd4ce837d93a7731438b3d45c4e933fbf
Puranjay Bhargava <90250628+puranjayb@users.noreply.github.com>
2023-06-28 15:50:35 +0530

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ import imagePuranjay from './images/PB.gif'
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>Credits@Puranjay Bhargava 2023</p>
+      <p>Credits @Puranjay Bhargava 2023</p>
       <img src={imagePuranjay} alt="Puranjay Image"></img>
     </footer>
   );
