2039ca15c41057cbde5cdc135a9d05da88d27f13
Puranjay Bhargava <90250628+puranjayb@users.noreply.github.com>
2023-06-28 15:48:39 +0530

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ import imagePuranjay from './images/PB.gif'
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>Credits <a href="https://github.com/puranjayb/srm-gpa-calculator-demo" target='_blank'>@Puranjay Bhargava</a> 2023</p>
+      <p>Credits@Puranjay Bhargava 2023</p>
       <img src={imagePuranjay} alt="Puranjay Image"></img>
     </footer>
   );
