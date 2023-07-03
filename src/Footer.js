a613669402f6b60899f687934f72290f5e1aa9c5
puranjayb <pa4257@srmist.edu.in>
2023-07-03 12:00:07 +0530

    Merge branch 'main' of https://github.com/puranjayb/srm-gpa-calculator-demo

--- src/Footer.js
+++ src/Footer.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Footer.css';
-import imagePuranjay from './images/PB.gif'
+import imagePuranjay from './images/PB.gif';
 
 const Footer = () => {
+  const [visitCount, setVisitCount] = useState(0);
+
+  useEffect(() => {
+    // Retrieve visit count from localStorage
+    let count = localStorage.getItem('page_view');
+    if (count) {
+      count = Number(count) + 1;
+    } else {
+      count = 1;
+    }
+    localStorage.setItem('page_view', count);
+    setVisitCount(count);
+
+    // Clean up localStorage and visit count on component unmount
+    return () => {
+      localStorage.removeItem('page_view');
+    };
+  }, []);
+
+  const handleReset = () => {
+    localStorage.removeItem('page_view');
+    setVisitCount(1);
+  };
+
   return (
     <footer className="footer">
-      <p>Credits &#169; Puranjay Bhargava 2023</p>
-      <img src={imagePuranjay} alt="Puranjay Image"></img>
+      <p>Credits &#169; Puranjay Bhargava 2023 </p>
+      <img src={imagePuranjay} alt="Puranjay Image" />
+      <div> {visitCount}</div>
     </footer>
   );
 };
