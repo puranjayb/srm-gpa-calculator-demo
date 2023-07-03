7a8fac9568159453c848c9ca880a07711f2d7907
puranjayb <pa4257@srmist.edu.in>
2023-07-03 11:28:39 +0530

    replace

--- src/Footer.js
+++ src/Footer.js
@@ -2,10 +2,11 @@ import React from 'react';
 import './Footer.css';
 import imagePuranjay from './images/PB.gif'
 
+
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>Credits <a href="https://github.com/puranjayb/srm-gpa-calculator-demo" target='_blank'>@Puranjay Bhargava</a> 2023</p>
+      <p>Credits <a href="https://github.com/puranjayb/srm-gpa-calculator-demo" target='_blank'>&#169; Puranjay Bhargava</a> 2023</p>
       <img src={imagePuranjay} alt="Puranjay Image"></img>
     </footer>
   );
